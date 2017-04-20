649bbb6a4df91bae1f83180a08d33f77c7c08355
Johannes Nickel <jn@znuny.com>
2017-04-21 00:51:34 +0200

    added locale setup for de and en

---
+++ conf.py
@@ -0,0 +1,14 @@
+locale_dirs = ['locale/']
+gettext_compact = False
+
+source_suffix = '.rst'
+
+master_doc = 'index'
+exclude_patterns = ['_build', 'html', 'doctrees']
+language = "de"
+
+import sphinx_rtd_theme
+
+html_theme = "sphinx_rtd_theme"
+
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
