9b816502947ac1256d977c6a8038e6ad5c558a3e
MrGeneration <github@thehomeofanime.de>
2020-05-25 19:03:40 +0200

    add overwrite_css
    This overwriting css files fixes too wide tables to fit into the content. This ensures that there are no vertical scroll bars which make it very hard to read the content itself.

--- conf.py
+++ conf.py
@@ -1,9 +1,7 @@
 import sys
 import os
 import time
-
 import sphinx_rtd_theme
-import time
 
 html_logo = "images/zammad_logo_70x61.png"
 html_favicon = "images/favicon.ico"
@@ -19,5 +17,21 @@ locale_dirs = ['locale/']
 gettext_compact = False
 language = "en"
 
-html_theme = "sphinx_rtd_theme"
+html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_static_path = ['_static']
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    # Override default css to solve issues (e.g. width, overflows)
+    def setup(app):
+        app.add_css_file('theme/theme_overrides.css')
+else:
+    # Override default css to solve issues (e.g. width, overflows)
+    html_context = {
+        'css_files': [
+            'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
+            'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
+            '_static/theme/theme_overrides.css'
+        ],
+    }
