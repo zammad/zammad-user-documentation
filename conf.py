e01f0465eb833f7d9041c49dc9a1f17c4b321a22
Marcel Herrguth <github@thehomeofanime.de>
2022-11-22 11:42:27 +0100

    Closes #102 - Sphinx tabs does not load required CSS

--- conf.py
+++ conf.py
@@ -38,7 +38,8 @@ else:
       'css_files': [
          'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
          'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
-         '_static/theme/theme_overrides.css'
+         '_static/theme/theme_overrides.css',
+         '_static/theme/tabs.css'
       ],
    }
 
